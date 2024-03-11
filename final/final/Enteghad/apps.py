6e88b79bc68645e67d98482e08362c4fb430e9e5
Ali-Hashemi10 <62511965+Ali-Hashemi10@users.noreply.github.com>
2024-03-11 15:23:26 -0400

    Add files via upload
    First Commit

---
+++ final/final/Enteghad/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class EnteghadConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "Enteghad"
---
+++ final/final/Enteghad/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout, authenticate
+from.models import User, Feedback
+
+# Create your views here.
+
+def user_register(request):
+    pass
+
+def user_login():
+    pass
+
+def user_logout():
+    pass
+
+def create_fb():
+    pass
+
+def user_fbs():
+    pass
