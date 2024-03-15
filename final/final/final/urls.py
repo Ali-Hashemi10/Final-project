591307eae46ec9cc1c5f70842a360216de7d2d11
amintehrani99 <160059247+amintehrani99@users.noreply.github.com>
2024-03-15 17:33:29 +0330

    Update urls.py

--- final/final/final/urls.py
+++ final/final/final/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from Enteghad import views
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("Enteghad", include=("Enteghad.urls")),
+    path("Enteghad/", include("Enteghad.urls")),
+    
 ]
